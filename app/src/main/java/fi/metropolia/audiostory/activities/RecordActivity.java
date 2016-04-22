3febf993bef44082b19bc2fba6b629462090e025
Vallo <wipsi7@gmail.com>
2016-04-22 20:12:04 +0300

    *Created PlayThread in separete class and assigned all play releated functions in class.

--- app/src/main/java/fi/metropolia/audiostory/activities/RecordActivity.java
+++ app/src/main/java/fi/metropolia/audiostory/activities/RecordActivity.java
@@ -1,8 +1,5 @@
 package fi.metropolia.audiostory.activities;
 
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -17,38 +14,29 @@ import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 
 import fi.metropolia.audiostory.R;
 import fi.metropolia.audiostory.filestorage.Folder;
 import fi.metropolia.audiostory.filestorage.RawFile;
+import fi.metropolia.audiostory.threads.PlayThread;
 import fi.metropolia.audiostory.threads.RecordThread;
 
 public class RecordActivity extends AppCompatActivity {
 
-
-
     private final String DEBUG_TAG = "RecordActivity";
 
-    private boolean playing = false;
     private boolean recordClicked = false;
-
-
 
     private File wavFile = null;
 
     private TextView titleTextView;
 
-    private RecordThread recordThread;
-    private Folder folder;
-    private RawFile rawFile;
-
+    private RecordThread recordThread = null;
+    private PlayThread playThread = null;
+    private Folder folder = null;
+    private RawFile rawFile = null;
 
 
     @Override
@@ -95,6 +83,16 @@ public class RecordActivity extends AppCompatActivity {
         }
     }
 
+    public void onPlayClick(View v){
+        if(rawFile.getFile() != null && rawFile.exists()){
+            if(playThread == null || !playThread.isPlaying()){
+                playThread = new PlayThread(rawFile);
+                playThread.start();
+                Toast.makeText(this, "Playing", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     public void onStopClick(View v) {
 
         if (recordClicked) {
@@ -104,66 +102,11 @@ public class RecordActivity extends AppCompatActivity {
         }
     }
 
-    public void onPlayClick(View v){
-        if(rawFile.getFile() != null && rawFile.exists()){
-            if(!playing){
-                Thread playThread = new Thread(){
-                    @Override
-                    public void run() {
-                       startPlaying();
-                    }
-                };
-                playThread.start();
-            }
-        }
-    }
 
     public void onUploadClick(View v){
         if(rawFile != null && rawFile.exists()){
             convWav();
-
         }
-
-    }
-
-    private void startPlaying(){
-
-        int minBufferSize = AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
-        AudioTrack track = new AudioTrack(
-                AudioManager.STREAM_MUSIC,
-                44100,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT,
-                minBufferSize,
-                AudioTrack.MODE_STREAM
-        );
-
-        playing = true;
-        track.play();
-
-
-        byte[] buffer = new byte[minBufferSize];
-
-
-        try {
-
-            InputStream inputStream = new FileInputStream(rawFile.getFile());
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-            DataInputStream dataInputStream = new DataInputStream(bufferedInputStream);
-
-            int i;
-            while ((i = dataInputStream.read(buffer, 0, minBufferSize)) != -1){
-                track.write(buffer,0,i);
-            }
-
-            dataInputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        track.stop();
-        track.release();
-        playing = false;
-
     }
 
 
